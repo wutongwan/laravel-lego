0e6c35aa05c7b0ff6e6fc5b0597b93762904f869
zhwei <zhwei.yes@gmail.com>
2016-08-24 22:15:42 +0800

    改善兼容性 & 自动提示

--- src/Lego/Field/Plugin/EloquentPlugin.php
+++ src/Lego/Field/Plugin/EloquentPlugin.php
@@ -1,5 +1,6 @@
 <?php namespace Lego\Field\Plugin;
 
+use Lego\Field\Field;
 use Lego\Source\Table\EloquentTable;
 
 /**
@@ -27,10 +28,11 @@ trait EloquentPlugin
     {
         $this->assertIsEloquentRecord();
 
+        /** @var Field $this */
         /** @var \Eloquent $model */
         $model = $this->source()->original();
 
-        $id = $id ?: $model->id ?: 'NULL';
+        $id = $id ?: $model->getKey() ?: 'NULL';
         $idColumn = $idColumn ?: $model->getKeyName();
 
         $parts = [
