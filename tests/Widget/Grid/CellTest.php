4e311ad849ebb2dcb4d1d1fd98348aa0eaa7de6e
zhwei <zhwei.yes@gmail.com>
2017-03-03 09:20:20 +0800

    fix timezone bug in UT

--- tests/Widget/Grid/CellTest.php
+++ tests/Widget/Grid/CellTest.php
@@ -36,18 +36,18 @@ class CellTest extends TestCase
             ['trim', ' 123 ', '123'],
             ['trim', ' 123', '123'],
 
-            ['date', 1488902400, '2017-03-08'],
+            ['date', Carbon::parse('2017-03-08')->timestamp, '2017-03-08'],
             ['date', '2017-03-08', '2017-03-08'],
             ['date', '2017-03-08 12:00:00', '2017-03-08'],
             ['date', Carbon::now(), Carbon::today()->format('Y-m-d')],
 
-            ['datetime', 1488902400, '2017-03-08 00:00:00'],
+            ['datetime', Carbon::parse('2017-03-08')->timestamp, '2017-03-08 00:00:00'],
             ['datetime', '2017-03-08', '2017-03-08 00:00:00'],
             ['datetime', '2017-03-08 12:00:00', '2017-03-08 12:00:00'],
             ['datetime', Carbon::now(), Carbon::now()->format('Y-m-d H:i:s')],
             ['datetime', 'now', Carbon::now()->format('Y-m-d H:i:s')],
 
-            ['time', 1488902401, '00:00:01'],
+            ['time', Carbon::parse('2017-03-08 00:00:01')->timestamp, '00:00:01'],
             ['time', '2017-03-08', '00:00:00'],
             ['time', '2017-03-08 12:00:00', '12:00:00'],
             ['time', Carbon::now(), Carbon::now()->format('H:i:s')],
