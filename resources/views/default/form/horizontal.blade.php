e84e16e47f82aa5c7ef3b41182946a8db441c6a3
zhwei <zhwei.yes@gmail.com>
2017-03-29 16:02:47 +0800

    拆分 Form view：Condition Group

--- resources/views/default/form/horizontal.blade.php
+++ resources/views/default/form/horizontal.blade.php
@@ -24,23 +24,5 @@
 @include('lego::default.snippets.bottom-buttons', ['widget' => $form])
 
 @push('lego-scripts')
-    @foreach($form->groups() as $group)
-        <?php /* @var \Lego\Field\Group $group */?>
-        @if(!$group->getCondition())
-            @continue
-        @endif
-
-        @foreach($group->fields() as $target)
-            <script>
-                $(document).ready(function () {
-                    var form = '{{ $form->elementId() }}';
-                    var field = '{{ $group->getCondition()->field()->elementName() }}';
-                    var operator = '{{ $group->getCondition()->operator() }}';
-                    var expected = JSON.parse('{!! json_encode($group->getCondition()->expected()) !!}');
-                    var target = '{{ $target->elementName() }}';
-                    (new LegoConditionGroup('#' + form, field, operator, expected, target)).watch();
-                })
-            </script>
-        @endforeach
-    @endforeach
+    @include('lego::default.form.condition-group', ['groups' => $form->groups()])
 @endpush
