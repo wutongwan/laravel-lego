9432159a22330807a99a8e03d0d138285a574af4
zhwei <zhwei.yes@gmail.com>
2017-06-28 08:06:13 +0800

    add UT helper: faker and FakeMobileDetect

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,8 @@
 namespace Lego\Tests;
 
 use Lego\Widget\Widget;
+use Lego\LegoServiceProvider;
+use Lego\Tests\Tools\FakeMobileDetect;
 
 abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
@@ -13,16 +15,35 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
 
         // register lego
         $app->register(\Collective\Html\HtmlServiceProvider::class);
-        $app->register(\Lego\LegoServiceProvider::class);
+        $app->register(LegoServiceProvider::class);
 
         // add test view namespace
         $app->make('view')->addNamespace('lego-test', __DIR__ . '/resources/views');
 
+        $app->bind(\Mobile_Detect::class, FakeMobileDetect::class);
+
         return $app;
+    }
+
+    protected function render2html(Widget $widget)
+    {
+        return $this->render($widget)->render();
     }
 
     protected function render(Widget $widget)
     {
         return $widget->view('lego-test::widget', ['widget' => $widget]);
+    }
+
+    protected function faker()
+    {
+        return \Faker\Factory::create($this->app->getLocale());
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        FakeMobileDetect::forgetMocks();
     }
 }
