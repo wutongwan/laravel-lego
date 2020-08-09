cae41f6abbc744ec3d07a1f3526b69aade13fff5
zhwei <zhwei.yes@gmail.com>
2020-08-09 20:17:14 +0800

    refactor: 添加外部脚本模板，方便接入时引用

---
+++ resources/views/default/external-scripts.blade.php
@@ -0,0 +1,4 @@
+<link href="/packages/wutongwan/lego/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<script src="/packages/wutongwan/lego/jquery/jquery.min.js"></script>
+<script src="/packages/wutongwan/lego/bootstrap/js/bootstrap.min.js"></script>
